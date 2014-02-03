7efb0cb8cbb20a8f52ec5e10a2f6924b2398964e
David Simon <david.mike.simon@gmail.com>
2014-02-03 11:08:43 -0500

    Setting up annotations cache earlier

--- ACModelTraitsBundle.php
+++ ACModelTraitsBundle.php
@@ -4,13 +4,15 @@ namespace AC\ModelTraitsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use AC\ModelTraitsBundle\DependencyInjection\ModelTraitsExtension;
 
 class ACModelTraitsBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
+        AnnotationReaderFetcher::setReaderFetchFunc(function() use ($container) {
+            return $container->get('annotation_reader');
+        });
+
         parent::build($container);
-        $container->registerExtension(new ModelTraitsExtension());
     }
 }
