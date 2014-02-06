3cc884b0ffff79a47ee2655c9ad73d699dbb9be2
David Simon <david.mike.simon@gmail.com>
2014-02-06 10:57:56 -0500

    Fixed boot function

--- ACModelTraitsBundle.php
+++ ACModelTraitsBundle.php
@@ -3,13 +3,17 @@
 namespace AC\ModelTraitsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use AC\ModelTraits\AnnotationReaderFetcher;
 
 class ACModelTraitsBundle extends Bundle
 {
     public function boot()
     {
+        $container = $this->container;
+        if (!($container instanceof ContainerInterface)) {
+            throw new \RuntimeException("Invalid container!\n");
+        }
         AnnotationReaderFetcher::setReaderFetchFunc(function() use ($container) {
             return $container->get('annotation_reader');
         });
